ceb240a8d7101b98e6811490cba51bfc864ac6ca
DrFaust92 <ilia.lazebnik@gmail.com>
2019-05-25 20:50:37 +0300

    add kms key & tag support sns topic module

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 resource "aws_sns_topic" "this" {
-  count = "${var.create_sns_topic}"
+  count = "${var.create_sns_topic && var.sns_topic_with_kms == 0 ? 1 : 0}"
 
   name = "${var.sns_topic_name}"
+
+  tags = "${var.tags}"
+}
+
+
+resource "aws_sns_topic" "this_with_kms" {
+  count = "${var.create_sns_topic && var.sns_topic_with_kms == 1 ? 1 : 0}"
+
+  name = "${var.sns_topic_name}"
+
+  kms_master_key_id = "${var.kms_master_key_id}"
+
+  tags = "${var.tags}"
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,18 @@ variable "create_sns_topic" {
 variable "sns_topic_name" {
   description = "The name of the SNS topic to create"
 }
+
+variable "sns_topic_with_kms" {
+  description = "Whether to create SNS topic with KMS encryption"
+  default     = false
+}
+
+variable "kms_master_key_id" {
+  description = "The ID of an AWS-managed customer master key (CMK) for Amazon SNS or a custom CMK"
+  default     = ""
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources"
+  default     = {}
+}
