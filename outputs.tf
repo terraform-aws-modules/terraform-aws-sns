e502a5f2d71a490d41927fd5c1a84037163388f7
Simone Ferraro <74993769+simoferr98@users.noreply.github.com>
2021-12-16 10:26:13 +0100

    feat: Added outputs sns_topic_name, sns_topic_id, sns_topic_owner (#27)
    Co-authored-by: Anton Babenko <anton@antonbabenko.com>

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "sns_topic_arn" {
   description = "ARN of SNS topic"
   value       = element(concat(aws_sns_topic.this.*.arn, [""]), 0)
 }
+
+output "sns_topic_name" {
+  description = "NAME of SNS topic"
+  value       = element(concat(aws_sns_topic.this.*.name, [""]), 0)
+}
+
+output "sns_topic_id" {
+  description = "ID of SNS topic"
+  value       = element(concat(aws_sns_topic.this.*.id, [""]), 0)
+}
+
+output "sns_topic_owner" {
+  description = "OWNER of SNS topic"
+  value       = element(concat(aws_sns_topic.this.*.owner, [""]), 0)
+}
