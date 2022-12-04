eb5b96574e9bd0bd6454dedb6e90eeaf3b694fe7
Edgar R. Sandi <edgar.cbbrasil@gmail.com>
2022-12-04 12:00:35 -0300

    feat: Add firehose args (#35)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>
    Closes https://github.com/terraform-aws-modules/terraform-aws-sns/issues/34

--- variables.tf
+++ variables.tf
@@ -52,6 +52,24 @@ variable "application_failure_feedback_role_arn" {
   default     = null
 }
 
+variable "firehose_success_feedback_role_arn" {
+  description = "The IAM role permitted to receive success feedback for this topic"
+  type        = string
+  default     = null
+}
+
+variable "firehose_success_feedback_sample_rate" {
+  description = "Percentage of success to sample"
+  type        = number
+  default     = null
+}
+
+variable "firehose_failure_feedback_role_arn" {
+  description = "IAM role for failure feedback"
+  type        = string
+  default     = null
+}
+
 variable "http_success_feedback_role_arn" {
   description = "The IAM role permitted to receive success feedback for this topic"
   type        = string
