cd47e39b26795f2d7ddccc77b246162fd6761e5c
Matheus Mazzoni <matheusdiasmazzoni@gmail.com>
2023-07-12 17:29:43 -0300

    refactor: update default try values on subscription on main module

--- main.tf
+++ main.tf
@@ -145,14 +145,14 @@ module "subscriptions" {
 
   for_each = { for k, v in var.subscriptions : k => v if var.create && var.create_subscription }
 
-  confirmation_timeout_in_minutes = try(each.value.confirmation_timeout_in_minutes, null)
+  confirmation_timeout_in_minutes = try(each.value.confirmation_timeout_in_minutes, 1)
   delivery_policy                 = try(each.value.delivery_policy, null)
   endpoint                        = each.value.endpoint
-  endpoint_auto_confirms          = try(each.value.endpoint_auto_confirms, null)
+  endpoint_auto_confirms          = try(each.value.endpoint_auto_confirms, false)
   filter_policy                   = try(each.value.filter_policy, null)
-  filter_policy_scope             = try(each.value.filter_policy_scope, null)
+  filter_policy_scope             = try(each.value.filter_policy_scope, "MessageAttributes")
   protocol                        = each.value.protocol
-  raw_message_delivery            = try(each.value.raw_message_delivery, null)
+  raw_message_delivery            = try(each.value.raw_message_delivery, false)
   redrive_policy                  = try(each.value.redrive_policy, null)
   subscription_role_arn           = try(each.value.subscription_role_arn, null)
   topic_arn                       = aws_sns_topic.this[0].arn
